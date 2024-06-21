1947c11c8769e7c5312db52f02a5795cb9d8487c
lokendra005 <141302553+lokendra005@users.noreply.github.com>
2024-06-21 10:09:35 +0530

    created context folder and CartContext

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import { a, b } from './components/Products/Products';
 import Products from './components/Products/Products';
 import { useState } from 'react';
+import CartContext from './context/CartContext';
 function App() {
   // state variable
   // inc
@@ -33,22 +34,13 @@ function App() {
 
   console.log(a, b);
   return (
+    <CartContext.Provider value={{ cart, increaseQuantity, decreaseQuantity }}>
     <div className="App">
       <Products cart={cart} increaseQuantity={increaseQuantity} decreaseQuantity={decreaseQuantity} />
     </div>
+    </CartContext.Provider>
   );
 }
 
 export default App;
 
-// {1:{id: 1, quantity: 11}, 2:{id: 2, quantity: 10}, 3:{id: 3, quantity: 10}, 4:{id: 4, quantity: 10}}
-
-// button
-// - 1 +
-
-// let a = {b:10, c:20};
-//a.b = 30;
-// let b = {...a};
-
-// a => 1234
-// a => 12
